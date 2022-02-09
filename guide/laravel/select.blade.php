7a295e3ea932332e43c8b1123bca9d64412c4b16
KValexander <ewoase@gmail.com>
2022-02-09 20:06:53 +0300

    guide upd

---
+++ guide/laravel/select.blade.php
@@ -0,0 +1,41 @@
+@extends("layout")
+
+<!-- 
+	web.php
+	--------------
+		Route::get("/select", [MainController::class, "select_page"]);
+
+	MainController.php
+	--------------
+		public function select_page() {
+			$content = DB::table("table")->get();
+			return view("select", ["content" => $content]);
+		}
+
+-->
+
+@section("content")
+	
+	<table>
+		<tr>
+			<th>id</th>
+			<th>Поле 1</th>
+			<th>Поле 2</th>
+			<th>Поле 3</th>
+		</tr>
+		@if(count($content))
+			@foreach($content as $value)
+				<tr>
+					<td>{{ $value->id }}</td>
+					<td>{{ $value->field_1 }}</td>
+					<td>{{ $value->field_2 }}</td>
+					<td>{{ $value->field_3 }}</td>
+				</tr>
+			@endforeach
+		@else
+			<tr><td colspan="4">Данные отсутствуют</td></tr>
+		@endif
+
+	</table>
+
+@endsection
